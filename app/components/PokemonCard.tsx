c95f9ec5b746e22cf0799232001d6ee0fba45349
Frederik Barbre <64972229+frbarbre@users.noreply.github.com>
2023-09-13 08:04:04 +0200

    Update PokemonCard.tsx

--- app/components/PokemonCard.tsx
+++ app/components/PokemonCard.tsx
@@ -7,7 +7,7 @@ export default function PokemonCard({ dexNumber, pokemon }: PokemonCard) {
     <Link href={`/${dexNumber}`}>
       <div className="w-[104px] h-[108px] flex flex-col border-white shadow-2dp items-center justify-between rounded-[7px] py-[4px] hover:shadow-inner-2dp transition-all">
         <h3 className="text-[8px] text-medium self-end pr-[8px]">#{dexNumber.length === 1 ? "00" : dexNumber.length === 2 ? "0" : ""}{dexNumber}</h3>
-        <Image
+        <img
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${dexNumber}.png`}
           alt={pokemon.name}
           width={72}
